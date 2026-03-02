4cc9a9a081e2db2332984332f7bfb1682aca16b7
dhanushkrishnappa <dhanushk2204@gmail.com>
2026-03-02 17:32:19 +0530

    committed from master

---
+++ copy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+mkdir dir1
+cd dir1
+echo "dgfdgf" >hello.txt
+mkdir dir2
+
+cp dir1/hello.txt dir2
+echo "copied successfully"
---
+++ sum.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "ENTER A NUMBER:"
+read num1
+echo "ENTER 2ND NUMBER:"
+read num2
+sum=$((num1+num2))
+echo "SUM IS: $sum"
